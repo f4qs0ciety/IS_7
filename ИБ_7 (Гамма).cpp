50a6d234052e7f2f3c3039bbae1f7dee4a02f44c
Егор Будюкин <wickedshaman@protonmail.com>
2022-04-06 20:22:56 +0300

    Добавлено гаммирование

--- ИБ_7 (Гамма).cpp
+++ ИБ_7 (Гамма).cpp
@@ -15,37 +15,64 @@ int charToBin(unsigned char val) //Начало функции
 	return bin; //Возврат двоичного значения
 }
 
+char binToChar(int bin) { //Начало функции
+	int k = 0;
+	for (int i = 0; i < 8; i++) { //Цикл побитового перевода двоичного числа
+		k = k + bin % 10 * pow(2, i);
+		bin = bin / 10;
+	}
+	char output = k;
+	return output; //Возврат символа
+}
+
+
 void encrypt() {
 	cout << endl << "Введите текст: ";
 	string input;
+	string bin_input;
+	int temp = 0;
 	cin.ignore();
 	getline(cin, input);
 	cout << "Введите ключ шифрования: ";
 	string key;
-	cin.ignore();
 	getline(cin, key);
 	for (int i = 0; i < input.length(); i++) {
-		input[i] = charToBin(input[i]);
+		temp = charToBin(input[i]);
+		for (int j = 7; j >= 0; j--) {
+			int temp2 = pow(10, j);
+			bin_input += temp / temp2 + '0';
+			temp = temp % temp2;
+		}
+	}
+	for (int i = 0; i < bin_input.length();) {
+		for (int j = 0; j < key.length(); j++) {
+			bin_input[i] = (bin_input[i] - '0') ^ (key[j] - '0') + '0';
+			i++;
+			if (i > bin_input.length()) break;
+		}
+	}
+	for (int i = 0; i < bin_input.length(); i += 8) {
+		int bin_symb = 0;
+		for (int j = i; j < i + 8; j++) {
+			bin_symb *= 10;
+			bin_symb += bin_input[j] - '0';
+		}
+		cout << binToChar(bin_symb);
 	}
 }
 
-void decrypt() {
-	
-};
-
 void main() {
-	setlocale(LC_ALL, "Russian");
+	//setlocale(LC_ALL, "Russian");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	int option; //Выбор опции меню
 	bool stop = false; //Индикатор остановки программы
 	do {
-		cout << endl << "Выберите режим работы: " << endl << "[1] Шифровка текста" << endl << "[2] Дешифровка текста" << endl << "[3] Выход" << endl << "Выбор: ";
+		cout << endl << "Выберите режим работы: " << endl << "[1] (Де)шифровка текста" << endl << "[2] Выход" << endl << "Выбор: ";
 		cin >> option;
 		switch (option) {
 		case 1: {encrypt(); break; }
-		case 2: {decrypt(); break; }
-		case 3: stop = true;
+		case 2: stop = true;
 		}
 	} while (stop != true);
 }
